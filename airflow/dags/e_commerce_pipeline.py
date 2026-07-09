c02eb286bd8d659ab84fafcde1f5231b525392f8
minhaz74692 <minhaz.ipe.sust@gmail.com>
2026-07-09 18:15:16 +0600

    Changes in the dag dependencies

--- airflow/dags/e_commerce_pipeline.py
+++ airflow/dags/e_commerce_pipeline.py
@@ -39,15 +39,14 @@ def ecommerce_lakehouse_elt_pipeline():
     fact_sales = databricks_notebook_task(config.fact_sales, config.NOTEBOOKS[config.fact_sales])
 
     # Analytics- SQL 
-    analytics_monthly = databricks_sql_notebook_task(config.monthly_revenue, config.NOTEBOOKS[config.monthly_revenue], config.WAREHOUSE_ID)
-    analytics_top_products = databricks_sql_notebook_task(config.top_products, config.NOTEBOOKS[config.top_products], config.WAREHOUSE_ID)
-    analytics_customer_retention = databricks_sql_notebook_task(config.customer_retention_cohort, config.NOTEBOOKS[config.customer_retention_cohort], config.WAREHOUSE_ID)
+    analytics_monthly = databricks_notebook_task(config.monthly_revenue, config.NOTEBOOKS[config.monthly_revenue])
+    analytics_top_products = databricks_notebook_task(config.top_products, config.NOTEBOOKS[config.top_products])
+    analytics_customer_retention = databricks_notebook_task(config.customer_retention_cohort, config.NOTEBOOKS[config.customer_retention_cohort])
 
 
     # Dependencies expressed as one readable block
-    bronze_tasks = [bronze_ingestion, bronze_reviews]
     silver_tasks = [silver_customers, silver_orders, silver_order_items,
-                    silver_products, silver_payments, silver_reviews]
+                    silver_products, silver_payments]
     dim_tasks = [dim_customer, dim_product, dim_date]
     analytics_tasks = [analytics_monthly, analytics_top_products, analytics_customer_retention]
 
@@ -60,7 +59,6 @@ def ecommerce_lakehouse_elt_pipeline():
     bronze_ingestion >> silver_tasks >> dq_silver >> dim_tasks >> fact_sales >> analytics_tasks
     bronze_reviews >> silver_reviews
 
-    # analytics_tasks
 
 
 
